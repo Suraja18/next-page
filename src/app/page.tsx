3c4f6bf3a03bcc8de25c086a7fbc93c46ca0fbbb
Suraj Adhikari <sssadhikari222@gmail.com>
2024-09-21 13:27:12 +0545

    React Hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import prisma from "@/lib/prisma";
 import Post from "../components/Post"
+import Link from "next/link";
 
 async function getPosts() {
   const posts = await prisma.post.findMany({
@@ -20,6 +21,11 @@ export default async function Home() {
   return (
     <div>
       <h1 className="font-bold mt-8 text-5xl text-center flex items-center justify-center">Posts</h1>
+      <Link href="/post/create-post">
+        <button className="float-right mt-6 mr-12 text-lg text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">
+          Add Post
+        </button>
+      </Link>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
